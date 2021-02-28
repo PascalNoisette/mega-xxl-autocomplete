c9181037335448f5caed6344db67030a2462aefd
Pascal Noisette <netpascal0123@aol.com>
2021-02-28 20:12:06 +0100

    Remove react flex to be responsive

--- src/ControlForm.js
+++ src/ControlForm.js
@@ -1,8 +1,6 @@
 import React, { Component }  from 'react';
 import { ReactiveBase, DataSearch } from '@appbaseio/reactivesearch';
-import { Grid, Row, Col } from 'react-flexbox-grid';
 import {  EditButton } from 'react-admin';
-const array_chunk = (array, chunk_size) => Array(Math.ceil(array.length / chunk_size)).fill().map((_, index) => index * chunk_size).map(begin => array.slice(begin, begin + chunk_size));
 
 class ControlForm extends Component {
 
@@ -36,7 +34,7 @@ class ControlForm extends Component {
   }
 
   render() {
-    const rows = array_chunk(this.services, 3);
+    const rows = [this.services];
     let index = 0;
     return (
         <div className="ControlForm">
@@ -44,15 +42,14 @@ class ControlForm extends Component {
                 <svg alt="Search" height="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15"><title>Search</title><path d=" M6.02945,10.20327a4.17382,4.17382,0,1,1,4.17382-4.17382A4.15609,4.15609, 0,0,1,6.02945,10.20327Zm9.69195,4.2199L10.8989,9.59979A5.88021,5.88021, 0,0,0,12.058,6.02856,6.00467,6.00467,0,1,0,9.59979,10.8989l4.82338, 4.82338a.89729.89729,0,0,0,1.29912,0,.89749.89749,0,0,0-.00087-1.29909Z "></path></svg>
                 <input type="text" value={this.state.value} onChange={this.handleChange} />
             </form>
-            <Grid fluid>
             {rows.map((row, j) => {
               const that = this;
               return (
-                <Row key={j}>
+                <div key={j}>
                     {row.map((service) => {
                       index++;
                   return (
-                      <Col key={index} xs={6} md={3}>
+                      <div key={index} className="cell">
                           <img 
                             style={{margin:"5px"}}
                             height="30px"
@@ -62,32 +59,27 @@ class ControlForm extends Component {
           url={""+window.location}
           app={service.app}
       >
+                              <EditButton basePath="/services" record={service}/>
                               <DataSearch
                   componentId={"searchbox" + index} 
                   debounce={500}
                   ref={(input) => {that.Progress.push(input) }}
                   dataField={service.dataField}
-                  categoryField="NotebookId.keyword"
-                  placeholder= {"Search for "+ service.app} 
                   downShiftProps={{
                     onSelect:  value => {
                       window.location = service.location + value.source[service.source]
                     }
                   }}
-                  style={{
-                      padding: '5px',
-                      marginTop: '10px',
-                  }}
               />
-                              <EditButton basePath="/services" record={service}/>
+                              
                           </ReactiveBase>
-                      </Col>
+                      </div>
                   )
                 })}
 
-                </Row>)})}
+                </div>)})}
 
-            </Grid>
+            
         </div>
     );
   }

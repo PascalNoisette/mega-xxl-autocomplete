a89ad304a4adbfa80a60f959e79608b9ea6391fa
Pascal Noisette <netpascal0123@aol.com>
2021-04-11 23:44:31 +0200

    Fix eslint

--- components/ReactAdmin/ServiceList/InlineGrid.tsx
+++ components/ReactAdmin/ServiceList/InlineGrid.tsx
@@ -4,40 +4,41 @@ import { useListContext } from 'ra-core';
 import PropTypes from 'prop-types';
 import { useDatagridStyles, DatagridLoading, DatagridProps } from 'react-admin';
 
-const InlineGrid: FC<DatagridProps & { filter?: (any) => any }> = React.forwardRef(
-    (props) => {
-        const classes = useDatagridStyles(props);
-        const { children, filter } = props;
+const InlineGrid: FC<
+    DatagridProps & { filter?: (any) => any; className?: string }
+> = React.forwardRef((props) => {
+    const classes = useDatagridStyles(props);
+    // eslint-disable-next-line react/prop-types
+    const { children, filter, className } = props;
 
-        const { data, ids, loaded, total } = useListContext(props);
+    const { data, ids, loaded, total } = useListContext(props);
 
-        if (loaded === false) {
-            return (
-                <DatagridLoading
-                    classes={classes}
-                    nbChildren={React.Children.count(children)}
-                />
-            );
-        }
-
-        if (loaded && (ids.length === 0 || total === 0)) {
-            return null;
-        }
+    if (loaded === false) {
         return (
-            <div className={props.className}>
-                {ids.map(
-                    (id, rowIndex) =>
-                        (!filter || filter(data[id])) &&
-                        React.isValidElement(children) &&
-                        cloneElement(children, {
-                            key: rowIndex,
-                            record: data[id]
-                        })
-                )}
-            </div>
+            <DatagridLoading
+                classes={classes}
+                nbChildren={React.Children.count(children)}
+            />
         );
     }
-);
+
+    if (loaded && (ids.length === 0 || total === 0)) {
+        return null;
+    }
+    return (
+        <div className={className}>
+            {ids.map(
+                (id, rowIndex) =>
+                    (!filter || filter(data[id])) &&
+                    React.isValidElement(children) &&
+                    cloneElement(children, {
+                        key: rowIndex,
+                        record: data[id]
+                    })
+            )}
+        </div>
+    );
+});
 
 InlineGrid.propTypes = {
     children: PropTypes.element,
